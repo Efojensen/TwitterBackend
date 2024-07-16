2ceb464439a2c7723c3621962ccbe21fac8950b9
Jensen <Jensena231@gmail.com>
2024-07-16 03:12:41 -0800

    Made my own changes to the backend

--- src/index.ts
+++ src/index.ts
@@ -5,10 +5,17 @@ import authRoutes from './routes/authRoutes';
 import { authenticateToken } from './middlewares/authMiddleware';
 
 const app = express();
+const cors = require('cors')
 app.use(express.json());
 app.use('/user', authenticateToken, userRoutes);
 app.use('/tweet', authenticateToken, tweetRoutes);
 app.use('/auth', authRoutes);
+app.use(cors())
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  next();
+})
 
 app.get('/', (req, res) => {
   res.send('Hello "updated" world!');
--- src/services/emailService.ts
+++ src/services/emailService.ts
@@ -1,48 +1,46 @@
-import { SESClient, SendEmailCommand } from '@aws-sdk/client-ses';
-import { error } from 'console';
-require('dotenv').config()
+import { SESClient, SendEmailCommand } from "@aws-sdk/client-ses";
+import { error } from "console";
+require ('dotenv').config();
 
-const ses = new SESClient({});
+const ses = new SESClient({
+    region: 'eu-north-1',
+    credentials: {
+        accessKeyId: process.env.AWS_ACCESS_KEY_ID!,
+        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY!
+    }
+});
 
-function createSendEmailCommand(
-  toAddress: string,
-  fromAddress: string,
-  message: string
-) {
-  return new SendEmailCommand({
-    Destination: {
-      ToAddresses: [toAddress],
-    },
-    Source: fromAddress,
-    Message: {
-      Subject: {
-        Charset: 'UTF-8',
-        Data: 'Your one-time password',
-      },
-      Body: {
-        Text: {
-          Charset: 'UTF-8',
-          Data: message,
+function createSendEmailCommand(toAddress: string, fromAddress: string, message: string){
+    return new SendEmailCommand({
+        Destination: {
+            ToAddresses: [toAddress],
         },
-      },
-    },
-  });
+        Source: fromAddress,
+        Message: {
+            Subject: {
+                Charset: 'UTF-8',
+                Data: "Your one time password"
+            },
+            Body: {
+                Text: {
+                    Charset: 'UTF-8',
+                    Data: message
+                }
+            },
+        }
+    })
 }
 
-export async function sendEmailToken(email: string, token: string) {
-  console.log('email: ', email, token);
+export async function sendEmailToken(email: string, token: string){
+    console.log("email: ", email, token);
 
-  const message = `Your one time password: ${token}`;
-  const command = createSendEmailCommand(
-    email,
-    'savinvadim1312@gmail.com',
-    message
-  );
+    const message = `Your one time password: ${token}`
+    const command = createSendEmailCommand(email, "sedemadjei419@gmail.com", message)
 
-  try {
-    return await ses.send(command);
-  } catch (e) {
-    console.log('Error sending email', e);
-    return error;
-  }
+    try{
+        return await ses.send(command);
+    }catch (e){
+        console.log("Error sending email", e);
+        return error;
+    }
 }
